51092073e8e8cda89cd8efe748a8795283cc74b6
Jaeheon Shim <git@jaeheonshim.com>
2023-06-07 14:32:22 -0400

    Append ready status to the end of the player list strings

--- server/model/classes.ts
+++ server/model/classes.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-enum CAHGameStatus {
+export enum CAHGameStatus {
     PLAYER_JOIN
 }
 
--- server/util.ts
+++ server/util.ts
@@ -1,11 +1,28 @@
 import { retrieveUsername } from "./manager/usernameManager";
-import { CAHGame } from "./model/classes";
+import { CAHGame, CAHGameStatus, CAHPlayer } from "./model/classes";
+
+function sortPlayers(p1: CAHPlayer, p2: CAHPlayer) {
+    if(p2.points == p1.points) {
+        const compare = retrieveUsername(p1.id).localeCompare(retrieveUsername(p2.id));
+        return compare;
+    } else {
+        return p2.points - p1.points;
+    }
+}
 
 export function getPlayerString(game: CAHGame) {
-    return Object.values(game.players).reduce<string>((prev, player) => {
+    return Object.values(game.players).sort(sortPlayers).reduce<string>((prev, player) => {
         const username = retrieveUsername(player.id);
         const points = player.points;
-        const str = `\`${username}\` - ${points} points`;
+        let str = `\`${username}\` - ${points} points`;
+        
+        if(game.status == CAHGameStatus.PLAYER_JOIN) {
+            if(player.ready) {
+                str += " (**READY**)";
+            } else {
+                str += " (Not Ready)";
+            }
+        }
 
         return prev + str + "\n";
     }, "").trimEnd();
