182bebb69a8eab7e62979edc5c01a8bb46181794
Jaeheon Shim <git@jaeheonshim.com>
2023-06-09 14:24:13 -0400

    Don't broadcastEval for user messages

--- bot/shardMessaging.ts
+++ bot/shardMessaging.ts
@@ -1,23 +1,20 @@
 import { Client, TextBasedChannel } from "discord.js";
 
 export async function sendMessageToUser(client: Client, userId: string, message) {
-    return await client.shard.broadcastEval(async (c, { userId, message }) => {
-        const user = await c.users.fetch(userId);
-        if(user) {
-            await user.send(message);
-            return true;
-        }
-        return false;
-    }, { context: { userId, message }});
+    const user = await client.users.fetch(userId);
+    if (user) {
+        return await user.send(message);
+    }
+    return false;
 }
 
 export async function sendMessageToChannel(client: Client, channelId: string, message) {
     return await client.shard.broadcastEval(async (c, { channelId, message }) => {
         const channel = (await c.channels.fetch(channelId) as TextBasedChannel);
-        if(channel) {
+        if (channel) {
             await channel.send(message);
             return true;
         }
         return false;
-    }, { context: { channelId, message }});
+    }, { context: { channelId, message } });
 }
