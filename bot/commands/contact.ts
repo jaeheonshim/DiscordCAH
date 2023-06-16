2da0580888bead15a48e6b54a94456a02fdc3dc7
Jaeheon Shim <git@jaeheonshim.com>
2023-06-16 12:35:38 -0400

    Add feedback modal and command

---
+++ bot/commands/contact.ts
@@ -0,0 +1,42 @@
+import {
+  ActionRowBuilder,
+  Interaction,
+  ModalActionRowComponentBuilder,
+  ModalBuilder,
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} from "discord.js";
+import config from "../config.json" assert { type: "json" };
+import { executeDefaultTextCommandServerRequest } from "../util.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("contact")
+    .setDescription("Contact the developer"),
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const modal = new ModalBuilder();
+    modal.setCustomId("contactModal");
+    modal.setTitle("Contact the Developer");
+
+    const textFeedbackInput = new TextInputBuilder()
+      .setCustomId("feedbackInput")
+      .setLabel("Enter feedback here")
+      .setPlaceholder(
+        "Enter anything you would like to send to the developer of the bot here."
+      )
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
+
+    const firstActionRow =
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+        textFeedbackInput
+      );
+    modal.addComponents(firstActionRow);
+
+    await interaction.showModal(modal);
+  },
+};
---
+++ bot/events/modalSubmit.ts
@@ -0,0 +1,53 @@
+import { BaseInteraction, ButtonInteraction, Events, GuildMember, ModalSubmitInteraction, TextBasedChannel } from "discord.js";
+import { executeDefaultTextCommandServerRequest, scheduleRoundBegin } from "../util.js";
+import axios from "axios";
+import { scheduleJob } from "node-schedule";
+import * as Sentry from "@sentry/node";
+import { sendMessageToChannel, sendMessageToUser } from "../shardMessaging.js";
+import config from "../config.json" assert {type: "json"};
+
+export default {
+    name: Events.InteractionCreate,
+    async execute(interaction: BaseInteraction) {
+        if(!interaction.isModalSubmit()) return;
+            const customId = (interaction as ModalSubmitInteraction).customId;
+
+            const transaction = Sentry.startTransaction({
+                name: `modal:${customId}`
+            });
+    
+            Sentry.setUser({
+                id: interaction.user.id,
+                username: interaction.user.username
+            });
+
+            try {
+                if(customId === "contactModal") {
+                    const feedback = interaction.fields.getTextInputValue("feedbackInput");
+
+                    const feedbackEmbed = {
+                        title: "User Feedback",
+                        author: {
+                            name: interaction.user.username + "#" + interaction.user.discriminator,
+                            icon_url: interaction.user.avatarURL()
+                        },
+                        fields: [
+                            {
+                                name: "Feedback Content",
+                                value: feedback
+                            }
+                        ],
+                        timestamp: new Date().toISOString()
+                    };
+
+                    await sendMessageToChannel(interaction.client, config.feedbackChannel, { embeds: [feedbackEmbed] });
+                    await interaction.reply({ content: "Your feedback was received successfully! ", ephemeral: true });
+                }
+            } catch (e) {
+                console.error("A modal submit error occurred. Error has been reported to sentry.");
+                Sentry.captureException(e);
+            } finally {
+                transaction.finish();
+            }
+    },
+};
