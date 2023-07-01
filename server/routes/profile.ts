c5eca1469bb00c930a13c64aab614155503515dc
Jaeheon Shim <git@jaeheonshim.com>
2023-07-01 00:10:03 -0400

    Treat win ratio float properly

--- server/routes/profile.ts
+++ server/routes/profile.ts
@@ -55,7 +55,7 @@ profileRouter.post("/me", function (req, res) {
                                 },
                                 {
                                     name: "Win Ratio",
-                                    value: `${user.statistics.totalPoints / user.statistics.totalSubmissions}`,
+                                    value: `${Math.trunc(100 * user.statistics.totalPoints / user.statistics.totalSubmissions)}%`,
                                     inline: true
                                 }
                             ]
